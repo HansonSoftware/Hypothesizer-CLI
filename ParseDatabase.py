ac1f18e2124ad1152736a489078d176f2f1475cb
Hayden Hanson <hansonhayd3n@gmail.com>
2023-06-26 22:40:00 -0400

    Added getHypothesisById

--- ParseDatabase.py
+++ ParseDatabase.py
@@ -7,6 +7,11 @@ file = open("hypotheses.json")
 data = json.load(file)
 
 
+# ================================================================================
+# ============================= EVIDENCE PARSING =================================
+# ================================================================================
+
+
 # allEvidence:
 # Input: None
 # Output: Prints all evidence objects in the JSON.
@@ -18,6 +23,11 @@ def allEvidence():
         print("\n", key, ":", evidence[key])
 
 
+# ================================================================================
+# ============================= HYPOTHESES PARSING ===============================
+# ================================================================================
+
+
 # allHypotheses:
 # Input: None
 # Output: Prints all hypotheses objects in the JSON.
@@ -25,7 +35,7 @@ def allHypotheses():
     hypotheses = data["hypotheses"]
     for i in range(0, len(hypotheses)):
         print(
-            "------------------------------------------------------------------------------------\n"
+            "--------------------------------------------------------------------------------\n"
         )
         print("%s:\n" % hypotheses[i]["id"].replace("_", " "))
         keys = hypotheses[i].keys()
@@ -67,9 +77,99 @@ def allHypotheses():
 
 # exampleHypothesis:
 # Input: String fileName
-# Output: Prints an example hypothesis in the JSON.
+# Output: Prints the first hypothesis in the DB.
 def exampleHypothesis():
-    print(data)
+    hypothesis = data["hypotheses"][0]
+    print("%s:\n" % hypothesis["id"].replace("_", " "))
+    keys = hypothesis.keys()
+    for key in keys:
+        section = hypothesis[key]
+        if key == "tags":
+            print("Tags:")
+            tagCount = 1
+            for tag in section:
+                print("\t%d." % tagCount, tag)
+                tagCount = tagCount + 1
+            continue
+        if key == "evidence":
+            print("Related Evidence:")
+            count = 1
+            for evidence in section:
+                print("\tEvidence %d." % count)
+                evidenceKeys = evidence.keys()
+                for evidenceKey in evidenceKeys:
+                    if evidenceKey == "HowToFix":
+                        print("\t\t%s:" % evidenceKey)
+                        steps = evidence[evidenceKey]["steps"]
+                        print("\t\tSteps:")
+                        stepCount = 1
+                        for j in range(0, len(steps)):
+                            stepKeys = steps[j].keys()
+                            print("\t\t\tStep %d." % stepCount)
+                            for stepKey in stepKeys:
+                                print("\t\t\t\t%s: %s" % (stepKey, steps[j][stepKey]))
+                            stepCount = stepCount + 1
+                        continue
+                    print("\t\t%s:" % evidenceKey, evidence[evidenceKey])
+                count = count + 1
+            continue
+        print("%s:" % key, section)
+
+
+# getHypothesisById:
+# Input: Integer id
+# Output: Prints the hypothesis at id
+def getHypothesisById(id):
+    found = False
+    hypotheses = data["hypotheses"]
+    for i in range(0, len(hypotheses)):
+        hypothesisId = hypotheses[i]["id"].replace("_", " ").split()
+        for token in hypothesisId:
+            if token.isnumeric():
+                if token == id:
+                    found = True
+                    hypothesis = data["hypotheses"][i]
+                    print("%s:\n" % hypothesis["id"].replace("_", " "))
+                    keys = hypothesis.keys()
+                    for key in keys:
+                        section = hypothesis[key]
+                        if key == "tags":
+                            print("Tags:")
+                            tagCount = 1
+                            for tag in section:
+                                print("\t%d." % tagCount, tag)
+                                tagCount = tagCount + 1
+                            continue
+                        if key == "evidence":
+                            print("Related Evidence:")
+                            count = 1
+                            for evidence in section:
+                                print("\tEvidence %d." % count)
+                                evidenceKeys = evidence.keys()
+                                for evidenceKey in evidenceKeys:
+                                    if evidenceKey == "HowToFix":
+                                        print("\t\t%s:" % evidenceKey)
+                                        steps = evidence[evidenceKey]["steps"]
+                                        print("\t\tSteps:")
+                                        stepCount = 1
+                                        for j in range(0, len(steps)):
+                                            stepKeys = steps[j].keys()
+                                            print("\t\t\tStep %d." % stepCount)
+                                            for stepKey in stepKeys:
+                                                print(
+                                                    "\t\t\t\t%s: %s"
+                                                    % (stepKey, steps[j][stepKey])
+                                                )
+                                            stepCount = stepCount + 1
+                                        continue
+                                    print(
+                                        "\t\t%s:" % evidenceKey, evidence[evidenceKey]
+                                    )
+                                count = count + 1
+                            continue
+                        print("%s:" % key, section)
+    if found == False:
+        print("Sorry, the hypothesis id you entered is not in the database yet.")
 
 
 # Close the file
@@ -78,5 +178,7 @@ file.close()
 # This allows for calling a specific funtion from the command line.
 # Usage: python3 ParseHypotheses.py functionName
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         globals()[sys.argv[1]]()
+    if len(sys.argv) == 3:
+        globals()[sys.argv[1]](sys.argv[2])
